ef85b506a3bba275f0703bfa4d618f3b7a678126
User <User@LAPTOP-OCEGBUHS>
2021-04-07 14:47:50 +0200

    modif champ et setter/getter

--- src/modele/element/Arme.java
+++ src/modele/element/Arme.java
@@ -11,40 +11,10 @@ package modele.element;
  */
 public class Arme extends Equipement {
     
-    private double bonus_agilite;
-    private double bonus_force;
-    
     public Arme(String nom, String description, double poids, double agilite, double force) {
         super(nom,description,poids);
         this.bonus_agilite = agilite;
         this.bonus_force = force;
-    }
-
-    public double getBonus_agilite() {
-        return bonus_agilite;
-    }
-
-    public void setBonus_agilite(double bonus_agilite) {
-        this.bonus_agilite = bonus_agilite;
-    }
-
-    public double getBonus_force() {
-        return bonus_force;
-    }
-
-    public void setBonus_force(double bonus_force) {
-        this.bonus_force = bonus_force;
-    }
-    
-    
-    
-    @Override
-    public double getModificateurAgilite() {
-        return this.bonus_agilite;
-    }
-    @Override
-    public double getModificateurForce() {
-        return this.bonus_force;
     }
     
     
