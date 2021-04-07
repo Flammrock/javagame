9ce07054fd55fa6b0295cf9baf68ecab953825a0
User <User@LAPTOP-OCEGBUHS>
2021-04-07 12:11:59 +0200

    modification de la classe Lieu    - ajout des getter/setter    - ajout d'un champ nom    - ajout d'une méthode ajoutePorteVers    - ajout d'une méthode recupererPorte

--- src/modele/element/Lieu.java
+++ src/modele/element/Lieu.java
@@ -1,13 +1,87 @@
-
 package modele.element;
 
 import java.util.List;
 
+public class Lieu extends Element {
+    
+    String nom;
 
-public class Lieu extends Element{
-  List<Porte> listePorte;
-  List<Objet> objets;
-  List<Personnage> monstres;
+    List<Porte> listePorte;
+    List<Objet> objets;
+    List<Personnage> monstres;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public List<Porte> getListePorte() {
+        return listePorte;
+    }
+
+    public void setListePorte(List<Porte> listePorte) {
+        this.listePorte = listePorte;
+    }
+
+    public List<Objet> getObjets() {
+        return objets;
+    }
+
+    public void setObjets(List<Objet> objets) {
+        this.objets = objets;
+    }
+
+    public List<Personnage> getMonstres() {
+        return monstres;
+    }
+
+    public void setMonstres(List<Personnage> monstres) {
+        this.monstres = monstres;
+    }
+
+    /**
+     * Chaque lieu doit avoir un nom unique
+     * @param nom le nom du lieu
+     */
+    public Lieu(String nom) {
+        this.nom = nom;
+    }
     
+    /**
+     * Permet de connecter deux lieux avec un porte
+     *
+     * @param nom le nom de la porte (attention le nom de la porte doit être unique)
+     * @param lieu2 le lieu à connecter
+     * @return retourne true si la porte a bien été ajouté, false sinon
+     */
+    public boolean ajoutePorteVers(String nom, Lieu lieu2) {
+        
+        // on regarde si une porte d'une même nom existe déjà
+        for (Porte p : this.listePorte) {
+            if (p.getNom().equals(nom)) return false;
+        }
+        for (Porte p : lieu2.getListePorte()) {
+            if (p.getNom().equals(nom)) return false;
+        }
+
+        Porte p1 = new Porte(nom, this, lieu2);
+        Porte p2 = new Porte(nom, lieu2, this);
+
+        this.listePorte.add(p1);
+        lieu2.getListePorte().add(p2);
+        
+        return true;
+    }
     
+    /**
+     * Permet de récupérer une porte via son nom
+     * @param nom le nom de la porte à récupérer
+     * @return retourne la porte sinon retourne null
+     */
+    public Porte recupererPorte(String nom) {
+        for (Porte p : this.listePorte) {
+            if (p.getNom().equals(nom)) return p;
+        }
+        return null;
+    }
+
 }
