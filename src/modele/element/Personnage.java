5edf1a3bebeac070500607c2c04f158f97889a99
Utilisateur <Utilisateur@TycoMoon>
2021-04-07 10:19:51 +0200

    ajout variable personnage

--- src/modele/element/Personnage.java
+++ src/modele/element/Personnage.java
@@ -1,10 +1,15 @@
 
 package modele.element;
 
+import java.util.ArrayList;
+
 
 public class Personnage extends Element  {
-private String Nom;
-
+private String nom;
 private int age;
+private int force;
+private int agilite;
+private int pv;
+private ArrayList<Objet> inventaire;
 
 }
