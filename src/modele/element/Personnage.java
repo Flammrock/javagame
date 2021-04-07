390b4079867c4aa2b1a78a4fa126ed5b3d57e428
Utilisateur <Utilisateur@TycoMoon>
2021-04-07 10:28:21 +0200

    ajout setteur getteur personnage

--- src/modele/element/Personnage.java
+++ src/modele/element/Personnage.java
@@ -11,5 +11,53 @@ private int force;
 private int agilite;
 private int pv;
 private ArrayList<Objet> inventaire;
+private Objet main;
+private Objet armure;
+private Lieu pieceActuel;
+
+    public int getPv() {
+        return pv;
+    }
+
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+
+    public ArrayList<Objet> getInventaire() {
+        return inventaire;
+    }
+
+    public void setInventaire(ArrayList<Objet> inventaire) {
+        this.inventaire = inventaire;
+    }
+
+    public Objet getMain() {
+        return main;
+    }
+
+    public void setMain(Objet main) {
+        this.main = main;
+    }
+
+    public Objet getArmure() {
+        return armure;
+    }
+
+    public void setArmure(Objet armure) {
+        this.armure = armure;
+    }
+
+    public Lieu getPieceActuel() {
+        return pieceActuel;
+    }
+
+    public void setPieceActuel(Lieu pieceActuel) {
+        this.pieceActuel = pieceActuel;
+    }
+
+
+
+
+
 
 }
