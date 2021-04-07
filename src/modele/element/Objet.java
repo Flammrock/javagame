41f1c6bc7518636c97957426090c8b74408dcaaa
User <User@LAPTOP-OCEGBUHS>
2021-04-07 14:00:49 +0200

    ajout constructeur

--- src/modele/element/Objet.java
+++ src/modele/element/Objet.java
@@ -6,6 +6,16 @@ public abstract class Objet extends Element {
     private String nom;
     private double poids;
     
+    public Objet() {
+        this.nom = "";
+        this.poids = 0.0;
+    }
+    
+    public Objet(String nom, double poids) {
+        this.nom = nom;
+        this.poids = poids;
+    }
+    
     public String effet(Personnage utilisateur, Element cible) {
         return "";
     }
