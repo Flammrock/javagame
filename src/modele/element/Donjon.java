5a8c606aceeba0ddcff7e764e9ca7a28a22b2d07
User <User@LAPTOP-OCEGBUHS>
2021-04-07 12:43:20 +0200

    ajout de la méthode ajouterLieu

--- src/modele/element/Donjon.java
+++ src/modele/element/Donjon.java
@@ -15,4 +15,8 @@ public class Donjon extends Element{
     private List<Lieu> salles;
     private Lieu entree;
     private Lieu sortie;
+
+    void ajouterLieu(Lieu lieu) {
+        salles.add(lieu);
+    }
 }
