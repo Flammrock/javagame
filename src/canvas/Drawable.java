9686a4a75dbea661485fd8de47345ccb56928c0e
User <User@LAPTOP-OCEGBUHS>
2021-04-07 23:11:55 +0200

    ajout de l'interface Drawable

---
+++ src/canvas/Drawable.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package canvas;
+
+import java.awt.Graphics;
+
+/**
+ *
+ * @author User
+ */
+public interface Drawable {
+    
+    void draw(Canvas c, Graphics g);
+    
+}
