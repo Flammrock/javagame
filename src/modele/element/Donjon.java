bc141dd3299f90e06a1a28ca301b8a13fecd29ec
Utilisateur <Utilisateur@TycoMoon>
2021-04-07 11:07:46 +0200

    ajout extends Element

--- src/modele/element/Donjon.java
+++ src/modele/element/Donjon.java
@@ -11,7 +11,7 @@ import java.util.List;
  *
  * @author Utilisateur
  */
-public class Donjon {
+public class Donjon extends Element{
     private List<Lieu> salles;
     private Lieu entree;
     private Lieu sortie;
