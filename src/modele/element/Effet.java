5b38eb7e512c7f280a8036a07ec93c5119114557
Utilisateur <Utilisateur@TycoMoon>
2021-04-08 08:03:31 +0200

    Ajout de Effet.java

---
+++ src/modele/element/Effet.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele.element;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Effet extends Element {
+    private String Nom;
+    private double duréeDeLEffet;
+    private double forceAjoute;
+    private double forceAjouteContinue;
+    private double agiliteAjoute;
+    private double agiliteAjouteContinue;
+    private double pvAjoute;
+    private double pvAjouteContinue;
+    private Lieu tpSalle;
+    private double poidsAjoute;
+
+    public Effet(String Nom,String description, double duréeDeLEffet, double forceAjoute, double forceAjouteContinue, double agiliteAjoute, double agiliteAjouteContinue, double pvAjoute, double pvAjouteContinue, Lieu tpSalle, double poidsAjoute) {
+        this.Nom = Nom;
+        this.description = description;
+        this.duréeDeLEffet = duréeDeLEffet;
+        this.forceAjoute = forceAjoute;
+        this.forceAjouteContinue = forceAjouteContinue;
+        this.agiliteAjoute = agiliteAjoute;
+        this.agiliteAjouteContinue = agiliteAjouteContinue;
+        this.pvAjoute = pvAjoute;
+        this.pvAjouteContinue = pvAjouteContinue;
+        this.tpSalle = tpSalle;
+        this.poidsAjoute = poidsAjoute;
+    }
+
+    
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public double getDuréeDeLEffet() {
+        return duréeDeLEffet;
+    }
+
+    public double getForceAjoute() {
+        return forceAjoute;
+    }
+
+    public double getForceAjouteContinue() {
+        return forceAjouteContinue;
+    }
+
+    public double getAgiliteAjoute() {
+        return agiliteAjoute;
+    }
+
+    public double getAgiliteAjouteContinue() {
+        return agiliteAjouteContinue;
+    }
+
+    public double getPvAjoute() {
+        return pvAjoute;
+    }
+
+    public double getPvAjouteContinue() {
+        return pvAjouteContinue;
+    }
+
+    public Lieu getTpSalle() {
+        return tpSalle;
+    }
+
+    public double getPoidsAjoute() {
+        return poidsAjoute;
+    }
+    
+    public boolean tourPasser(){
+        if(this.duréeDeLEffet!=-1){
+            this.duréeDeLEffet--;
+        }
+        return true;
+    }
+}
