ff7859885337c6c88eb4170fc73a29b59103aa7e
Utilisateur <Utilisateur@TycoMoon>
2021-04-08 06:32:41 +0200

    modif du looting lors de la mort

--- src/modele/element/Personnage.java
+++ src/modele/element/Personnage.java
@@ -218,8 +218,6 @@ public class Personnage extends Element {
     private void mort(Personnage ennemie) {
         ennemie.setPv(0.0);
         this.pieceActuel.ajouterPlusieursObjet(ennemie.getInventaire());
-        this.pieceActuel.ajouterObjet(ennemie.getMain());
-        this.pieceActuel.ajouterObjet(ennemie.getArmure());
     }
     
     
