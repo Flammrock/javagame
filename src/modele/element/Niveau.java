2ff353b166273a368801699d266dc488b9eb42d4
User <User@LAPTOP-OCEGBUHS>
2021-04-08 19:56:15 +0200

    création de la classe Niveau

---
+++ src/modele/element/Niveau.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele.element;
+
+/**
+ * Un niveau contient un début et une fin
+ * @author User
+ */
+public class Niveau {
+    
+}
