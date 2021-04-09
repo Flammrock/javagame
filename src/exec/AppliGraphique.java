d77dc3128bd479fc4d6cc7f41010bdb2e9f4d09c
User <User@LAPTOP-OCEGBUHS>
2021-04-09 06:57:59 +0200

    modification dispatcher keyevent

--- src/exec/AppliGraphique.java
+++ src/exec/AppliGraphique.java
@@ -824,12 +824,9 @@ public class AppliGraphique extends javax.swing.JFrame {
         @Override
         public boolean dispatchKeyEvent(KeyEvent e) {
             if (e.getID() == KeyEvent.KEY_PRESSED) {
-                //System.out.println("tester");
-                //canvas1.onAppuiTouche(e);
+                canvas1.onAppuiTouche(e);
             } else if (e.getID() == KeyEvent.KEY_RELEASED) {
-                //System.out.println("2test2");
-            } else if (e.getID() == KeyEvent.KEY_TYPED) {
-                //System.out.println("3test3");
+                canvas1.onRelacheTouche(e);
             }
             return false;
         }
