417225773e240cea44581f31f682e5189d096b85
User <User@LAPTOP-OCEGBUHS>
2021-04-09 05:43:49 +0200

    ajout de la méthode onAppuiTouche

--- src/canvas/Canvas.java
+++ src/canvas/Canvas.java
@@ -48,8 +48,10 @@ public class Canvas extends JPanel {
         }
         
     }
-    
-    
+
+    public void onAppuiTouche(KeyEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 
 }
