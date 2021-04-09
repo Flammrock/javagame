fab16cc540a57d4915417d7d3caac4d0de1b2ce1
User <User@LAPTOP-OCEGBUHS>
2021-04-09 05:44:34 +0200

    implementation de la méthode onAppuiTouche

--- src/canvas/Canvas.java
+++ src/canvas/Canvas.java
@@ -50,7 +50,7 @@ public class Canvas extends JPanel {
     }
 
     public void onAppuiTouche(KeyEvent e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("bojnour keyevent depuis canvas!");
     }
     
 
