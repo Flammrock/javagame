11a2ab2347e9602347188a36afb60213c9d9fcd1
Utilisateur <Utilisateur@TycoMoon>
2021-04-09 06:29:35 +0200

    maj des varaible en ArrayList

--- src/modele/element/Effet.java
+++ src/modele/element/Effet.java
@@ -5,45 +5,34 @@
  */
 package modele.element;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Utilisateur
  */
 public class Effet extends Element {
     private String Nom;
-    private double[] duréeDeLEffet = new double[2];
-    private double[] forceAjoute = new double[3];   //tableau ou l'indice: 0=effetCourant, 1=effetInitial, 2=effetContinu
-    private double[] agiliteAjoute = new double[3];
-    private double[] pvAjoute = new double[3];
-    private double[] pvMaxAjoute = new double[3];
+    private ArrayList<Double> duréeDeLEffet = new ArrayList<Double>();
+    private ArrayList<Double> forceAjoute = new ArrayList<Double>();   //tableau ou l'indice: 0=effetCourant, 1=effetInitial, 2=effetContinu
+    private ArrayList<Double> agiliteAjoute = new ArrayList<Double>();
+    private ArrayList<Double> pvAjoute = new ArrayList<Double>();
+    private ArrayList<Double> pvMaxAjoute = new ArrayList<Double>();
     private Lieu tpSalle;
-    private double[] poidsAjoute = new double[3];
-    private double[] armureAjoute = new double[3];
+    private ArrayList<Double> poidsAjoute = new ArrayList<Double>();
+    private ArrayList<Double> armureAjoute = new ArrayList<Double>();
 
     public Effet(String Nom,String description, double duréeDeLEffet, double forceAjoute, double agiliteAjoute, double pvAjoute, Lieu tpSalle, double poidsAjoute) {
         this.Nom = Nom;
         this.description = description;
-        this.duréeDeLEffet[0] = duréeDeLEffet;
-        this.duréeDeLEffet[1] = duréeDeLEffet;
-        this.forceAjoute[0] = forceAjoute;
-        this.forceAjoute[1] = forceAjoute;
-        this.forceAjoute[2] = 0;
-        this.agiliteAjoute[0] = agiliteAjoute;
-        this.agiliteAjoute[1] = agiliteAjoute;
-        this.agiliteAjoute[2] = 0;
-        this.pvAjoute[0] = pvAjoute;
-        this.pvAjoute[1] = pvAjoute;
-        this.pvAjoute[2] = 0;
-        this.pvMaxAjoute[0] = 0;
-        this.pvMaxAjoute[1] = 0;
-        this.pvMaxAjoute[2] = 0;
+        this.duréeDeLEffet.add(0,duréeDeLEffet);
+        this.forceAjoute.add(0,forceAjoute);
+        this.agiliteAjoute.add(0,agiliteAjoute);
+        this.pvAjoute.add(0,pvAjoute);
+        this.pvMaxAjoute.add(0,(double)0);
         this.tpSalle = tpSalle;
-        this.poidsAjoute[0] = poidsAjoute;
-        this.poidsAjoute[1] = poidsAjoute;
-        this.poidsAjoute[2] = 0;
-        this.armureAjoute[0] = 0;   //a ajouter
-        this.armureAjoute[1] = 0;
-        this.armureAjoute[2] = 0;
+        this.poidsAjoute.add(0,poidsAjoute);
+        this.armureAjoute.add(0,(double)0);
     }
 
     
@@ -53,19 +42,19 @@ public class Effet extends Element {
     }
 
     public double getDuréeDeLEffet() {
-        return duréeDeLEffet[0];
+        return duréeDeLEffet.get(0);
     }
 
     public double getForceAjoute() {
-        return forceAjoute[0];
+        return forceAjoute.get(0);
     }
 
     public double getAgiliteAjoute() {
-        return agiliteAjoute[0];
+        return agiliteAjoute.get(0);
     }
 
     public double getPvAjoute() {
-        return pvAjoute[0];
+        return pvAjoute.get(0);
     }
 
     public Lieu getTpSalle() {
@@ -77,15 +66,15 @@ public class Effet extends Element {
     }
 
     public double getPoidsAjoute() {
-        return poidsAjoute[0];
+        return poidsAjoute.get(0);
     }
     
     public double getPvMaxAjoute() {
-        return pvMaxAjoute[0];
+        return pvMaxAjoute.get(0);
     }
     
     public double getArmureAjoute() {
-        return armureAjoute[0];
+        return armureAjoute.get(0);
     }
     
     /**
@@ -93,30 +82,30 @@ public class Effet extends Element {
      * @return retourn true si l'effet doit rester actif et false si l'effet est fini est doit etre detruit
      */
     public boolean tourPasse(){
-        if(duréeDeLEffet[0]==duréeDeLEffet[1] && duréeDeLEffet[0]==-1){//effet permanant (l'effet restera actif pour toujours)
+        if(duréeDeLEffet.get(0)==-1){//effet permanant (l'effet restera actif pour toujours)
             tpSalle = null;
             return true;
         }
-        if(duréeDeLEffet[0]==duréeDeLEffet[1] && duréeDeLEffet[0]<-1){//effet instantané (l'effet se detruira)
+        if(duréeDeLEffet.get(0)<-1){//effet instantané (l'effet se detruira)
             return false;
         }
-        if(duréeDeLEffet[0]==duréeDeLEffet[1] && duréeDeLEffet[0]>-1){//effet a durée limité dans le temps(premiere fois que on utilise cette effet)
+        if(duréeDeLEffet.get(0)>=0){//effet a durée limité dans le temps(premiere fois que on utilise cette effet)
             tpSalle = null;
         }
-        if(duréeDeLEffet[0] == 1 ){//fin de l'effet a durée limité
-            forceAjoute[0] = 0;
-            agiliteAjoute[0] = 0;
-            pvAjoute[0] = 0;
-            pvMaxAjoute[0] = 0;
+        if(duréeDeLEffet.get(0) == 1 ){//fin de l'effet a durée limité
+            forceAjoute.add(0,(double)0);
+            agiliteAjoute.add(0,(double)0);
+            pvAjoute.add(0,(double)0);
+            pvMaxAjoute.add(0,(double)0);
             tpSalle = null;
-            poidsAjoute[0] = 0;
-            armureAjoute[0] = 0;
-            duréeDeLEffet[0] = 0;
+            poidsAjoute.add(0,(double)0);
+            armureAjoute.add(0,(double)0);
+            duréeDeLEffet.add(0,(double)0);
             return false;
         }
         
-        if(this.duréeDeLEffet[0]>=0){
-            this.duréeDeLEffet[0]--;
+        if(this.duréeDeLEffet.get(0)>=0){
+            this.duréeDeLEffet.add(0,this.duréeDeLEffet.get(0)-1);
         }
         return true;
     }
@@ -124,7 +113,7 @@ public class Effet extends Element {
     @Override
     public String toString(){
         String listesEffets = getNom()+" \n";
-        if(duréeDeLEffet[0]>=0){
+        if(duréeDeLEffet.get(0)>=0){
             if(getDuréeDeLEffet()>0){
                 listesEffets += "Durée : "+(int)getDuréeDeLEffet() + " Tours \n"; 
             }
