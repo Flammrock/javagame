f63d2af3e7a7db319d1fe778a9f712c8fecb6f0a
User <User@LAPTOP-OCEGBUHS>
2021-04-09 05:08:09 +0200

    mise à jour du main

--- src/exec/Prog.java
+++ src/exec/Prog.java
@@ -136,6 +136,7 @@ public class Prog {
         joueur.ajouter(hommeDeFer);
         joueur.ajouter(hommeDeFer);
         joueur.ajouter(hommeDeFer);
+        joueur.ajouter(potion);
         
         
         
