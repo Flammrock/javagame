2f7870aeeb634f536077af782c35a82e634a0ac8
User <User@LAPTOP-OCEGBUHS>
2021-04-12 20:15:32 +0200

    update du main

--- src/exec/Prog.java
+++ src/exec/Prog.java
@@ -146,13 +146,13 @@ public class Prog {
         joueur.ajouter(hommeDeFer);
         joueur.ajouter(new Effet("Homme de fer","augmente la force",false,false,3,5,0,0,0,0,0,new Lieu("Rien")));
         joueur.ajouter(potion);
-        joueur.setCollisionBox(new CollisionBox(0,32,42,32));
+        joueur.setCollisionBox(new CollisionBox(0,35,28,14));
         
         
         // on créé le sprite pour le joueur
-        SpriteSheet s = new SpriteSheet("/HerosSpriteSheet.png",0,0,64,64,64,64);
+        SpriteSheet s = new SpriteSheet("/HerosSpriteSheet.png",0,0,64,64,64*3,64*3);
         s.loadImage();
-        s.setDecal(25,23,64-36,64-38);
+        s.setDecal(27,23,27,23);
         s.ajouterAnimation(new Animation("Idle-Left",new int[] {0, 1, 2, 3}));
         s.ajouterAnimation(new Animation("Idle-Up",new int[] {4, 5, 6, 7}));
         s.ajouterAnimation(new Animation("Idle-Right",new int[] {8, 9, 10, 11}));
