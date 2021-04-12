bae7436b0f8afcc4aed23b25da0243aa5f068959
User <User@LAPTOP-OCEGBUHS>
2021-04-12 12:02:18 +0200

    ajout de getX,getX,setX,setY,MoveTo,MoveBy

--- src/canvas/Drawable.java
+++ src/canvas/Drawable.java
@@ -15,4 +15,18 @@ public interface Drawable {
     
     void draw(Canvas c, Graphics g);
     
+    public int getX();
+
+    public void setX(int x);
+
+    public int getY();
+
+    public void setY(int y);
+    
+    
+    
+    public void MoveTo(int x, int y);
+    
+    public void MoveBy(int x, int y);
+    
 }
