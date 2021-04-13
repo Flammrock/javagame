cfb5b8318cde6247887489ba28b580f3abd5653e
User <User@LAPTOP-OCEGBUHS>
2021-04-13 22:37:14 +0200

    ajout de la classe SimpleEvent dans le package eventsystem

---
+++ src/eventsystem/SimpleEvent.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventsystem;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author User
+ */
+public class SimpleEvent<T> implements Serializable {
+    
+    private T data;
+    
+    T getData() {
+        return this.data;
+    }
+    
+    public SimpleEvent(T data) {
+        this.data = data;
+    }
+    
+}
