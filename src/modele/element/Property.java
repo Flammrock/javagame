e1106e93a0654a703132f6e3282c480f2d487d09
Utilisateur <Utilisateur@TycoMoon>
2021-04-14 17:11:10 +0200

    ajout de property

---
+++ src/modele/element/Property.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele.element;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Property {
+    private String nom;
+    private double valeur;
+    
+}
