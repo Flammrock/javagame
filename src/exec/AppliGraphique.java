54f65c64d0d0d20f5cd7a8f5c2507fc11d109d98
Flammrock <flammrockgame@gmail.com>
2021-04-26 19:02:00 +0200

    ajout de isPauseDraw

--- src/exec/AppliGraphique.java
+++ src/exec/AppliGraphique.java
@@ -63,7 +63,7 @@ public class AppliGraphique extends javax.swing.JFrame {
         
         new Timer("Drawer", true).scheduleAtFixedRate( new TimerTask(){
             public void run(){
-               canvas1.repaint();
+               if (!_this.aventure.isPauseDraw()) canvas1.repaint();
                combatGraphique22.mettreAJourCanvas();
             }
         }, 
