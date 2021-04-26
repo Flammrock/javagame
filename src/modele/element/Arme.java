9fc72ef9bbc878c8285152c4bce2dc5e8b091749
Flammrock <flammrockgame@gmail.com>
2021-04-26 21:39:49 +0200

    les armes ont maintenant un sprite associé

--- src/modele/element/Arme.java
+++ src/modele/element/Arme.java
@@ -5,17 +5,34 @@
  */
 package modele.element;
 
+import canvas.Canvas;
+import canvas.Sprite;
+import java.awt.Graphics;
+
 /**
  *
  * @author User
  */
 public class Arme extends Equipement {
     
+    Sprite sprite;
+    
     public Arme(String nom, String description, double poids, double agilite, double force) {
         super(nom,description,poids);
+        this.sprite = null;
         this.bonus_agilite = agilite;
         this.bonus_force = force;
     }
     
+    public void setSprite(Sprite s) {
+        this.sprite = s.copie();
+    }
+    
+    @Override
+    public void draw(Canvas c, Graphics g) {
+        if (this.sprite == null) return;
+        
+        this.sprite.draw(c, g);
+    }
     
 }
