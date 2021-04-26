bb9add8daa1d8233f11e6ba7299e00700e2b0454
Flammrock <flammrockgame@gmail.com>
2021-04-26 21:41:39 +0200

    override de quelques méthodes dans Arme

--- src/modele/element/Arme.java
+++ src/modele/element/Arme.java
@@ -29,6 +29,52 @@ public class Arme extends Equipement {
     }
     
     @Override
+    public int getX() {
+        if (this.sprite == null) return 0;
+        return this.sprite.getX(); // pour l'instant, on propage ça dans le sprite
+    }
+
+    @Override
+    public void setX(int x) {
+        if (this.sprite == null) return;
+        this.sprite.setX(x); // pour l'instant, on propage ça dans le sprite
+    }
+
+    @Override
+    public int getY() {
+        if (this.sprite == null) return 0;
+        return this.sprite.getY(); // pour l'instant, on propage ça dans le sprite
+    }
+
+    @Override
+    public void setY(int y) {
+        if (this.sprite == null) return;
+        this.sprite.setY(y); // pour l'instant, on propage ça dans le sprite
+    }
+    
+    public int getWidth() {
+        if (this.sprite == null) return 0;
+        return this.sprite.computeWidth();
+    }
+    
+    public int getHeight() {
+        if (this.sprite == null) return 0;
+        return this.sprite.computeHeight();
+    }
+
+    @Override
+    public void moveTo(int x, int y) {
+        if (this.sprite == null) return;
+        this.sprite.moveTo(x,y); // pour l'instant, on propage ça dans le sprite
+    }
+
+    @Override
+    public void moveBy(int x, int y) {
+        if (this.sprite == null) return;
+        this.sprite.moveBy(x,y); // pour l'instant, on propage ça dans le sprite
+    }
+    
+    @Override
     public void draw(Canvas c, Graphics g) {
         if (this.sprite == null) return;
         
