1156ae8d210caf14d601c62e3869decf5aff8395
Utilisateur <Utilisateur@TycoMoon>
2021-05-05 10:02:29 +0200

    nouvelle invterface Rarity

---
+++ src/modele/element/Rarity.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele.element;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public interface Rarity {
+    public int COMMON = 0;
+    public int RARE = 1;
+    public int EPIC = 2;
+    public int LEGENDARY = 3;
+}
