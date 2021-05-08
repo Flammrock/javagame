1e285405ef4bbb78c5db3db4216942cd73f1f3aa
Utilisateur <Utilisateur@TycoMoon>
2021-05-08 15:28:46 +0200

    ajout de nom, description et poid en fonction de la level dans le constructeur

--- src/modele/element/Morceau.java
+++ src/modele/element/Morceau.java
@@ -15,7 +15,26 @@ public class Morceau extends Objet {
     private int level;
 
     public Morceau(int level) {
+        switch(level){
+            case(Rarity.COMMON):
+                this.nom = "Morceau commun";
+                this.description = "Utile pour réparer et construre";
+            break;
+            case(Rarity.RARE):
+                this.nom = "Morceau rare";
+                this.description = "Utile pour la construction avancé";
+            break;
+            case(Rarity.EPIC):
+                this.nom = "Morceau epic";
+                this.description = "Utile pour la construction avancé";
+            break;
+            case(Rarity.LEGENDARY):
+                this.nom = "Morceau legendaire";
+                this.description = "Utile pour la construction avancé";
+            break;
+        }
         this.level = level;
+        this.poids = 1;
     }
 
     public int getLevel() {
