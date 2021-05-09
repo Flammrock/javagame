57275d8bb7fa5af09585675351410c5a08adff3a
Utilisateur <Utilisateur@TycoMoon>
2021-05-09 21:01:16 +0200

    Ajout des drop d'armes aleatoire

--- src/modele/element/Arme.java
+++ src/modele/element/Arme.java
@@ -13,8 +13,13 @@ import canvas.collision.CollisionEvent;
 import canvas.collision.Collisionable;
 import eventsystem.Dispatcher;
 import eventsystem.SimpleListener;
+import exec.Settings;
 import java.awt.Graphics;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 import map.Generable;
 import map.GenerateListener;
 
@@ -26,6 +31,36 @@ public class Arme extends Equipement {
     
     public Arme(String nom, String description, double poids) {
         super(nom,description,poids);
+    }
+    
+    
+    public void initAleatoire(){
+        String[] mots;
+        InputStream in = getClass().getResourceAsStream("/armes");
+        try (Scanner scan = new Scanner(in)) {
+            int nbrLigne = 42;  //A modifier a chaque modification du fichier montres
+            Random random = new Random();
+            int LigneRdm = random.nextInt(nbrLigne);
+            scan.nextLine();
+            for(int i = 0;i<LigneRdm-1;i++){
+                scan.nextLine();
+            }
+            String phrase = scan.nextLine();
+            mots = phrase.split("/");
+            this.nom = mots[0];
+            this.description = mots[1];
+            Effet effetArme = new Effet("", "", true, false, 0);
+            effetArme.setProperty(PropertyList.FORCE, Double.parseDouble(mots[3]));
+            this.effet = effetArme;
+            Sprite sprite_epee = new Sprite("/items/Item__01.png",0,0,24,24);
+            sprite_epee.addCollisionBox(new CollisionBox(0, 0, 24, 24,Settings.DEBUG,false));
+            setSprite(sprite_epee);
+            setIcon(sprite_epee.getFileName());
+        }
+        try {
+            in.close();
+        } catch (IOException ex) {}
+        
     }
 
     @Override
